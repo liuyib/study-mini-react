7c9eb93bc0389c497400ef2be7686b01725a5a01
liuyibo <liuyibo@xylink.com>
2022-10-07 19:44:26 +0800

    refactor: rename file and dir

--- src/index.jsx
+++ src/index.jsx
@@ -3,4 +3,4 @@
  * 1. 替换了 React.createElement, ReactDOM.render
  * 2. 告诉 Babel 用我们自定义的 createElement 方法来编译 JSX
  */
-import './step2-render-function/1-2-3.jsx';
+import './step2-render-function/2-3.jsx';
