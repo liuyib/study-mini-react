30c7b33137e47e8e8893822da3b4281567f37485
liuyibo <liuyibo@xylink.com>
2022-10-08 00:13:52 +0800

    feat: add 'concurrent mode' support

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 /**
  * 最小实现
- * 1. 替换了 React.createElement, ReactDOM.render
+ * 1. 手动实现了 React.createElement, ReactDOM.render 方法
  * 2. 告诉 Babel 用我们自定义的 createElement 方法来编译 JSX
  */
 import './step2-render-function/2-3.jsx';
---
+++ src/step3-concurrent-mode/1.jsx
@@ -0,0 +1,89 @@
+/**
+ * 在开始实现 Concurrent Mode 之前，先重构下代码：
+ * 1. 调整 render 中的递归实现（如果递归生成的元素树过大，会阻塞浏览器渲染）
+ * 2. 用循环代替递归，以便可以停止任务
+ */
+
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) =>
+        typeof child === 'object' ? child : createTextElement(child),
+      ),
+    },
+  };
+}
+
+function createTextElement(text) {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+function render(element, container) {
+  const dom =
+    element.type === 'TEXT_ELEMENT'
+      ? document.createTextNode('')
+      : document.createElement(element.type);
+
+  const isProperty = (key) => key !== 'children';
+
+  // 传入的参数赋值到 DOM 上
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach((name) => {
+      dom[name] = element.props[name];
+    });
+
+  // 递归处理子项
+  element.props.children = element.props.children.map((child) =>
+    render(child, dom),
+  );
+
+  container.appendChild(dom);
+}
+
+let nextUnitOfWork = null;
+
+// 在浏览器空闲时间执行任务，没有空闲时间则放弃执行
+function workLoop(idleDeadline) {
+  let shouldYield = false;
+
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performNextUnitOfWork(nextUnitOfWork);
+    // 1: 1ms，同 requestIdleCallback 回调函数接收的参数中 timeRemaining() 返回值的单位
+    shouldYield = idleDeadline.timeRemaining() < 1;
+  }
+
+  window.requestIdleCallback(workLoop);
+}
+
+window.requestIdleCallback(workLoop);
+
+// 在浏览器空闲时间需要执行的任务
+function performNextUnitOfWork(nextUnitOfWork) {
+  // TODO: ...
+}
+
+const Didact = {
+  createElement,
+  render,
+};
+
+/** @jsx Didact.createElement */
+const element = (
+  <div id="foo">
+    <a>bar</a>
+    <b />
+  </div>
+);
+
+const container = document.getElementById('root');
+
+Didact.render(element, container);
