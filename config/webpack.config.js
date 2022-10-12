75ada54b783c71a5fd2b1501e66f3f7163592b53
liuyibo <liuyibo@xylink.com>
2022-10-12 23:08:48 +0800

    build(webpack): plugin that handle build assets only enable in production

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,4 +1,4 @@
-// const webpack = require('webpack');
+const webpack = require('webpack');
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin').CleanWebpackPlugin;
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -6,7 +6,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
 const paths = require('./paths');
 
-// const __DEV__ = process.env.NODE_ENV === 'development';
+const __DEV__ = process.env.NODE_ENV === 'development';
 const __PRD__ = process.env.NODE_ENV === 'production';
 
 module.exports = {
@@ -24,15 +24,6 @@ module.exports = {
       directory: path.join(__dirname, 'public'),
     },
   },
-  // optimization: {
-  //   splitChunks: {
-  //     chunks: 'all',
-  //     name: false,
-  //   },
-  //   runtimeChunk: {
-  //     name: (entrypoint) => `runtime-${entrypoint.name}`,
-  //   },
-  // },
   module: {
     rules: [
       {
@@ -44,28 +35,31 @@ module.exports = {
   },
   plugins: [
     // Clean output dir before rebuild.
-    new CleanWebpackPlugin(),
+    __PRD__ && new CleanWebpackPlugin(),
     // Copy all assets from `public` dir to output dir.
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: 'public/**/*',
-          to: '[name][ext]',
-          globOptions: {
-            ignore: ['**/index.html'],
+    __PRD__ &&
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: 'public/**/*',
+            to: '[name][ext]',
+            globOptions: {
+              ignore: ['**/index.html'],
+            },
           },
-        },
-      ],
-    }),
+        ],
+      }),
     // Generates an `index.html` file with the <script> injected.
-    new HtmlWebpackPlugin({
-      inject: true,
-      template: path.resolve(process.cwd(), 'public/index.html'),
-    }),
+    __PRD__ &&
+      new HtmlWebpackPlugin({
+        inject: true,
+        template: path.resolve(process.cwd(), 'public/index.html'),
+      }),
     // Makes the public URL available as %PUBLIC_URL% in index.html,
     // e.g.: <link rel="icon" href="%PUBLIC_URL%/favicon.ico">
-    new InterpolateHtmlPlugin(HtmlWebpackPlugin, {
-      PUBLIC_URL: paths.publicUrlOrPath.slice(0, -1),
-    }),
+    __PRD__ &&
+      new InterpolateHtmlPlugin(HtmlWebpackPlugin, {
+        PUBLIC_URL: paths.publicUrlOrPath.slice(0, -1),
+      }),
   ].filter(Boolean),
 };
